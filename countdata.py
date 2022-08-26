685f9e75ff9f57f34c298f9e96dcb662a600f7ae
Vivek Kumar <kumarvivek9088@gmail.com>
2022-08-26 17:43:59 +0000

    commit

---
+++ countdata.py
@@ -0,0 +1,31 @@
+from msilib.schema import Directory
+import os
+directory='Image/'
+count = {
+             'a': len(os.listdir(directory+"/A")),
+             'b': len(os.listdir(directory+"/B")),
+             'c': len(os.listdir(directory+"/C")),
+             'd': len(os.listdir(directory+"/D")),
+             'e': len(os.listdir(directory+"/E")),
+             'f': len(os.listdir(directory+"/F")),
+             'g': len(os.listdir(directory+"/G")),
+             'h': len(os.listdir(directory+"/H")),
+             'i': len(os.listdir(directory+"/I")),
+             'j': len(os.listdir(directory+"/J")),
+             'k': len(os.listdir(directory+"/K")),
+             'l': len(os.listdir(directory+"/L")),
+             'm': len(os.listdir(directory+"/M")),
+             'n': len(os.listdir(directory+"/N")),
+             'o': len(os.listdir(directory+"/O")),
+             'p': len(os.listdir(directory+"/P")),
+             'q': len(os.listdir(directory+"/Q")),
+             'r': len(os.listdir(directory+"/R")),
+             's': len(os.listdir(directory+"/S")),
+             't': len(os.listdir(directory+"/T")),
+             'u': len(os.listdir(directory+"/U")),
+             'v': len(os.listdir(directory+"/V")),
+             'w': len(os.listdir(directory+"/W")),
+             'x': len(os.listdir(directory+"/X")),
+             'y': len(os.listdir(directory+"/Y")),
+             'z': len(os.listdir(directory+"/Z"))
+             }
